484d2287c410e0dbe92f86229ecca6fa4fe228db
Nils Nilsson <nilspontusnilsson@gmail.com>
2020-03-18 09:00:24 +0100

    feat: add better logging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cron = require('node-cron')
 const got = require('got')
 const knex = require('knex')
 const config = require('./config')
+const log = require('node-pretty-log')
 
 const db = knex({
   client: 'pg',
@@ -15,35 +16,48 @@ const db = knex({
 })
 
 const getData = async () => {
-  const { body } = await got('https://www.svt.se/special/articledata/2322/sverige.json')
-  return JSON.parse(body)
+  try {
+    const { body } = await got('https://www.svt.se/special/articledata/2322/sverige.json')
+    return JSON.parse(body)
+  } catch (err) {
+    log('error', 'getData', err)
+  }
 }
 
 const savePointInTime = async (data) => {
-  return await db('point_in_time').insert({
-    total: data.total,
-    data_updated: new Date(data.data_updated)
-  }, ['*'])
+  try {
+    return await db('point_in_time').insert({
+      total: data.total,
+      data_updated: new Date(data.data_updated)
+    }, ['*'])
+  } catch (err) {
+    log('error', 'savePointInTime', err)
+  }
 }
 
 const savePointInTimeRegion = async (currentPointInTimeId, regions) => {
   regions.map(async region => {
-    await db('point_in_time_region').insert({
-      point_in_time_id: currentPointInTimeId,
-      infected: region.antal,
-      dead: region.dead,
-      area_code: region.kod,
-      area_name: region.namn,
-    })
+    try {
+      await db('point_in_time_region').insert({
+        point_in_time_id: currentPointInTimeId,
+        infected: region.antal,
+        dead: region.dead,
+        area_code: region.kod,
+        area_name: region.namn,
+      })
+    } catch (err) {
+      log('error', 'savePointInTimeRegion', err)
+    }
   })
 }
 
 cron.schedule(config.default.cron, async () => {
   const data = await getData()
   const [currentPointInTime] = await savePointInTime(data)
-  await savePointInTimeRegion(currentPointInTime.id, data.data)
 
-  console.log('saved', new Date())
+  savePointInTimeRegion(currentPointInTime.id, data.data)
+
+  log('success', 'data saved')
 })
 
-console.log('service started')
+log('info', 'service started')
